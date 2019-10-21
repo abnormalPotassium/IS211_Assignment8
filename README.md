695c60d48f423819537c3cca09e56761bee25894
alu-potato <taha098@gmail.com>
2019-10-20 21:58:53 -0500

    Implemented AI

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # IS211_Assignment 8
 
-OOP - The Pig Game now with AI!
+OOP - The Pig Game now with AI and Timed Mode!
 
-Allows for more than two players and also allows for more than one game in a row
+Allows for more than two human players using the --numPlayersHuman parameter and also allows for more than one game in a row.
+
+Additionally now allows for implementing AI players using the --numPlayersComputer parameter while also allowing for implementing a timed game mode using the --timed parameter.
--- pig.py
+++ pig.py
@@ -3,15 +3,20 @@ import random
 random.seed(0)
 
 parse = argparse.ArgumentParser()
-parse.add_argument('--numPlayers', nargs='?', default = '2')
+parse.add_argument('--numPlayersHuman', nargs='?', default = '0')
+parse.add_argument('--numPlayersComputer', nargs='?', default = '0')
+parse.add_argument('--timed', nargs='?')
 args = parse.parse_args()
-numPlayers = int(args.numPlayers)
+
+numPlayersHuman = int(args.numPlayersHuman)
+numPlayersComputer = int(args.numPlayersComputer)
+numPlayers = numPlayersHuman + numPlayersComputer
 
 if numPlayers < 2:
     print('You can\'t play Pig with only a single player just yet. Would you like to continue with 2 players?\n')
     response = input('Yes or No:  \n').lower()
     if response == 'yes' or response == 'y':
-        numPlayers = 2
+        numPlayersHuman = 2
     else:
         raise SystemExit
 
@@ -86,6 +91,48 @@ class player:
             x.reset_turn_total()
             x.overall_score = 0
 
+class computerPlayer(player):
+
+    def strategy(self):
+        if self.turn_total >= 25 or self.overall_score + self.turn_total >= 100:
+            print(f'{self.name} decides to play it safe and hold')
+            return 'h'
+        else:
+            print(f'{self.name} decides to live a little and roll!')
+            return 'r'
+    
+    def turn(self):
+        self.step_count += 1
+        while self.step_count > 0:
+            if self.step_count == 1:
+                print(f'\nIt\'s your turn {self.name}! Your total score is {self.overall_score} and it\'s the first step of your turn. What would you like your next action to be?\n')
+                self.step_count += 1
+            elif self.overall_score + self.turn_total >= 100:
+                print(f'\n{self.name} you last rolled {self.previous_roll}, your turn total is {self.turn_total} and your total score is {self.overall_score}. You have enough points to win if you hold!\n')
+                self.step_count += 1        
+            else:
+                print(f'\n{self.name} you last rolled {self.previous_roll}, your turn total is {self.turn_total} and your total score is {self.overall_score}. What would you like your next action to be?\n')
+                self.step_count += 1
+            while True:
+                decision = self.strategy()
+                if decision == 'r' or decision == 'r':
+                    self.previous_roll = dice.roll()
+                    if self.previous_roll == 1:
+                        print(f'\nUh oh, you rolled a one {self.name}. There go all {self.turn_total} points down the the drain! This ends your turn.\n')
+                        self.reset_turn_total()
+                        break
+                    else:
+                        self.turn_total += self.previous_roll
+                        print(f'\nNice! You rolled a {self.previous_roll} bringing you up to {self.turn_total} points!')
+                        break
+                elif decision == 'h' or decision == 'hold':
+                    self.gain_score()
+                    break
+                elif decision == 'e' or decision == 'exit':
+                    raise SystemExit
+                else:
+                    print(f'\nThat\'s not a valid input {self.name}. Input \'r\' to roll, \'h\' to hold, or \'e\' to exit.\n')
+        
 class die:
     def __init__(self,sides = 6):
         self.sides = sides
@@ -101,9 +148,12 @@ if consent == 'yes' or consent == 'y':
     winner = False
     newgame = False
     print('\nExcellent! Let\'s begin! First let me know your names.')
-    player_names = [input(f'\nPlayer {x} please enter your name:  \n') for x in range(1,numPlayers+1)]
-    for y in player_names:
+    player_names_human = [input(f'\nPlayer {x} please enter your name:  \n') for x in range(1,numPlayersHuman+1)]
+    player_names_computer = [f'AI#{x}' for x in range(1,numPlayersComputer+1)]
+    for y in player_names_human:
         y = player(y)
+    for y in player_names_computer:
+        y = computerPlayer(y)
     while winner == False:
         for z in player._registry:
             if winner == False and newgame == False:
